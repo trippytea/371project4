98bee5efa24bb774da42d91066a82b73dfb05f15
Alex Gachassin <alexgachassin@gmail.com>
2022-11-13 15:25:56 -0600

    updated character length requirements for project

--- social.sql
+++ social.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE users (
-    username VARCHAR(60) NOT NULL,
-    fName VARCHAR(60) NOT NULL,
-    lName VARCHAR (60) NOT NULL,
-    email VARCHAR(60) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    fName VARCHAR(50) NOT NULL,
+    lName VARCHAR (50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL,
     profilePic CHAR(60) NOT NULL,
    	PRIMARY KEY (username)
@@ -13,7 +13,7 @@ CREATE TABLE post (
     postId int NOT NULL AUTO_INCREMENT,
     postContent LONGTEXT NOT NULL,
     shortTitle CHAR(16) NOT NULL,
-    username VARCHAR(60) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     PRIMARY KEY(postId),
     FOREIGN KEY (username) REFERENCES users(username)
 );
@@ -22,7 +22,7 @@ CREATE TABLE comment (
     commentId int NOT NULL AUTO_INCREMENT,
     commentContent LONGTEXT NOT NULL,
     postId int NOT NULL,
-    username VARCHAR(60) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     PRIMARY KEY(commentId),
     FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (postId) REFERENCES post(postId)
@@ -31,7 +31,7 @@ CREATE TABLE comment (
 CREATE TABLE postlike (
     likeId int NOT NULL AUTO_INCREMENT,
     postId int NOT NULL,
-    username VARCHAR(60) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     PRIMARY KEY(likeId),
     FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (postId) REFERENCES post(postId)
@@ -40,7 +40,7 @@ CREATE TABLE postlike (
 CREATE TABLE commentlike (
     likeId int NOT NULL AUTO_INCREMENT,
     commentId int NOT NULL,
-    username VARCHAR(60) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     PRIMARY KEY(likeId),
     FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (commentId) REFERENCES comment(commentId)
