1ffdb3084926fea4d2de6eb7b7ff06850a48dd39
Alex Gachassin <alexgachassin@gmail.com>
2022-11-16 13:51:00 -0600

    updated footer

--- login.php
+++ login.php
@@ -96,5 +96,5 @@ if (is_password_correct($email, $password, $db)) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
-<footer class="centerContent">Copyright &copy 2022 Social Name???</footer>
+<footer class="centerContent">Copyright &copy 2022 The Goblin Den</footer>
 </html>
