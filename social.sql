326f1bf4c562ed37b7243005856963ab138f810b
Alex Gachassin <alexgachassin@gmail.com>
2022-11-23 12:00:30 -0600

    dates for comments, autocreate friends and posts

--- social.sql
+++ social.sql
@@ -32,6 +32,7 @@ CREATE TABLE comment (
     commentContent LONGTEXT NOT NULL,
     postId int NOT NULL,
     username VARCHAR(50) NOT NULL,
+    date DATETIME NOT NULL,
     PRIMARY KEY(commentId),
     FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (postId) REFERENCES post(postId)
@@ -55,3 +56,16 @@ CREATE TABLE commentlike (
     FOREIGN KEY (commentId) REFERENCES comment(commentId)
 );
 
+INSERT INTO users (username, fname, lname, email, password, profilePic) 
+VALUES ('Grugg', 'Grug', 'Gruggerson', 'grug@hotmail.com', '$2y$10$7na2EnNVM.3jckVYm0dcyuwoPAOKL9R/LwWc/e6jzv/9NgZvijR1i', 'goblin5.png'),
+        ('Krugg', 'Kris', 'Gruggerson', 'krug@hotmail.com', '$2y$10$RvhSpji66SiN9up8EfZ5y.D0QSpzrMgVk8w1.ZJJWhdsBuMxrpjpm', 'goblin2.png'),
+        ('Goblin_Tom', 'Tommy', 'McGoblin', 'tom@myspace.com', '$2y$10$bzF8EmkA2xY/6Yg2VTSuqOyL8w25nbe51azppUizXBFX8OfWpMyZK', 'goblin4.png');
+
+INSERT INTO friends (friendUsername, username)
+VALUES ('Grugg','Grugg'),('Krugg','Krugg'),('Goblin_Tom','Goblin_Tom');
+
+INSERT INTO post (postContent, username, date)
+VALUES ('Goblin do no bad. Goblin only want to eat trash and take shiny gold. Why people no like goblin? World make Grugg sad. . . :(', 
+         'Grugg', now()),
+       ('KRUUUUUUUUUGGGGGG IS DA BEST, OH YEAH BABY!!!!', 'Krugg', now()),
+       ('Welcome to the den, fellow goblin!', 'Goblin_Tom', now());
