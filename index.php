4589ecc5a145b5fad2c12acc029cf7c0b8285422
Alex Gachassin <alexgachassin@gmail.com>
2022-11-25 12:45:59 -0600

    adjusted card margins

--- index.php
+++ index.php
@@ -140,11 +140,11 @@ if (isset ($_POST['commentBtn'])) {
 		<div class="col-12 col-md-6 col-lg-4 order-1 order-md-1 order-lg-1 mb-2  centerContent">
             <div class="card mt-0 mt-lg-2 userBox" style="width: 21rem; height: 9.85rem;">
 			<a href='user-home.php?name=<?=$name?>' style='text-decoration:none;'>
-			<div class="card-body centerContent">
+			<div class="card-body centerContent my-4">
 				<span>
 				<img class='profileCard me-2' src="images\<?=$pic?>" height="112px" width="112px" alt="goblin" style="margin-top: 10px;">
 				<div style="float:right;"> 
-						<p style="color: #e9f6f1; letter-spacing:.75px; white-space:nowrap; display:inline-block;">
+						<p class='info mt-3' style="color: #e9f6f1; letter-spacing:.75px; white-space:nowrap; display:inline-block;">
 							<strong><?=$firstName." ".$lastName?></strong><br>
 							<strong><?=$name?></strong><br>
 							<strong>Posts: <?=$postTotal?></strong><br>
@@ -161,13 +161,16 @@ if (isset ($_POST['commentBtn'])) {
 	<div class="col-12 col-md-6 col-lg-8 order-2 order-md-2 order-lg-2">
 		<div>
 		<div class="card-body">
+		<!--post form-->
 		<h2>Create Post </h2>
 					<form method='post' action="index.php" class='text-end'> 
-						<input type="text" name='newPost' id='newPost' class="card-body w-100"  placeholder="Got something to say?"
+						<input type="text" name='newPost' id='newPost' class="card-body w-100"  placeholder=" Got something to say?"
 						style='height:50px;'>	
 						<button class="btn-primary btn-lg btn-block mt-2 " type="submit" name='submit'>Post</button>
 					</form>
 			</div>
+
+			<!--post feed-->
 			<div class="col-12 col-md-6 col-lg-8 order-2 order-md-2 order-lg-2 text-center text-lg-start" style="width:800px;"> <!--post styling-->
 				<span>
 					<?php
